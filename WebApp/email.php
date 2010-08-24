ad6b576f3f051656a39edf6aee5dea519f4917ea
snkashis <snkashis@gmail.com>
2010-08-24 13:30:43 +0100

    Aug24 Near Final

--- WebApp/email.php
+++ WebApp/email.php
@@ -3,28 +3,35 @@
     include ("config.php");    
     $to = trim($_GET['email']);
     
-    
-    
-    if ($email== 'guest' ){
-    $clear_current_user = "UPDATE Couple SET current = '0'";
-    $set_current_user = "UPDATE Couple SET current='1' WHERE id='guest'";
-    mysql_query($clear_current_user) or die(mysql_error());
-    mysql_query($set_current_user) or die(mysql_error());
-    echo 'SUCCESS, guest is now CURRENT';
-    }
-    
-    elseif ($email=='') {
-    echo 'nothing';
-    }
-    else {
-            $clear_current_user = "UPDATE Couple SET current = '0'";
-            $addClient  = "INSERT INTO Couple (id,current) VALUES ('$email','1')";
-            
-            mysql_query($clear_current_user) or die(mysql_error());
+//  
+$result = mysql_query("SELECT * FROM Results");
 
-            mysql_query($addClient) or die(mysql_error());
-            echo 'SUCCESS, new user is now current';
-  }
+
+if (!$result) die ("MySQL failure: " . mysql_error());
+
+$fields = mysql_num_fields($result);
+//echo 'num of fields ' . $fields . '<br/>';
+$row = mysql_fetch_row($result);
+
+echo '{ ';
+
+for ($a = 0 ; $a < $fields ; ++$a)
+{
+    if ($a >=1) {
+        echo ', ';
+    }
+    $field = mysql_field_name($result, $a);
+    
+    echo '"' . $field . '": "' . $row[$a] . '"';
+    
+    }
+    
+
+echo ' }';
+
+//
+    
+    
   
   
 $subject = "Contact Us";
--- WebApp/set_responses.php
+++ WebApp/set_responses.php
@@ -2,23 +2,82 @@
   
     include ("config.php");    
     
-    $did_shake = $_GET['shout'];
-    $did_shout = $_GET['shake'];
+    $did_resp_shake = $_GET['resp_shake'];
+    $did_resp_shout = $_GET['resp_shout'];
     
-    $set_shake = "UPDATE Results SET resp_shake='1'";
-    $set_shout = "UPDATE Results SET resp_shout='1'";
+    $did_ab_chin = $_GET['ab_chin'];
+    $did_ab_breathing = $_GET['ab_breathing'];
+    $did_ab_telephone = $_GET['ab_telephone'];
     
+    $did_cpr_compressions = $_GET['cpr_compressions'];
+    $did_cpr_chin = $_GET['cpr_chin'];
+    $did_cpr_nose = $_GET['cpr_nose'];
+    $did_cpr_breaths = $_GET['cpr_nose'];
+    //
     
-    if ($did_shake=='1'){
-    mysql_query($set_shake) or die(mysql_error());
+    $set_resp_shake = "UPDATE Results SET resp_shake='1'";
+    $set_resp_shout = "UPDATE Results SET resp_shout='1'";
+    
+    $set_ab_chin = "UPDATE Results SET ab_chin='1'";
+    $set_ab_breathing = "UPDATE Results SET ab_breathing='1'";
+    $set_ab_telephone = "UPDATE Results SET ab_telephone='1'";
+    
+    $set_cpr_compressions = "UPDATE Results SET cpr_compressions='1'";
+    $set_cpr_chin = "UPDATE Results SET cpr_chin='1'";
+    $set_cpr_nose = "UPDATE Results SET cpr_nose='1'";
+    $set_cpr_breaths = "UPDATE Results SET cpr_breaths='1'";
+  
+    if ($did_resp_shake=='1'){
+    mysql_query($set_resp_shake) or die(mysql_error());
     
     echo 'SUCCESS, resp_shake is now 1';
     }
     
     
-    if ($did_shout=='1') {
-    mysql_query($set_shout) or die(mysql_error());        
+    if ($did_resp_shout=='1') {
+    mysql_query($set_resp_shout) or die(mysql_error());        
     echo 'SUCCESS, resp_shout is now 1';    
     }
+    //
+    if ($did_ab_chin=='1'){
+    mysql_query($set_ab_chin) or die(mysql_error());
+    
+    echo 'SUCCESS, ab_chin is now 1';
+    }
+    
+    
+    if ($did_ab_breathing=='1') {
+    mysql_query($set_ab_breathing) or die(mysql_error());        
+    echo 'SUCCESS, ab_breathing is now 1';    
+    }
+    
+    if ($did_ab_telephone=='1') {
+    mysql_query($set_ab_telephone) or die(mysql_error());        
+    echo 'SUCCESS, ab_telephone is now 1';    
+    }
+    //
+    if ($did_cpr_compressions=='1') {
+    mysql_query($set_cpr_compressions) or die(mysql_error());        
+    echo 'SUCCESS, cpr_compressions is now 1';    
+    }
+    
+    if ($did_cpr_chin=='1'){
+    mysql_query($set_cpr_chin) or die(mysql_error());
+    
+    echo 'SUCCESS, cpr_chin is now 1';
+    }
+    
+    
+    if ($did_cpr_nose=='1') {
+    mysql_query($set_cpr_nose) or die(mysql_error());        
+    echo 'SUCCESS, cpr_nose is now 1';    
+    }
+    
+    if ($did_cpr_breaths=='1') {
+    mysql_query($set_cpr_breaths) or die(mysql_error());        
+    echo 'SUCCESS, cpr_breaths  is now 1';    
+    }
+    
+    
     
 ?>  
